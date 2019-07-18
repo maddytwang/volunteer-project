3498709ba64b6279e5b759782e9be3ad812af520
Maddy Wang <maddywang@Maddys-MacBook-Air.local>
2019-07-18 10:03:31 -0700

    tracker update 2

--- tracker/tracker.js
+++ tracker/tracker.js
@@ -1,86 +1,94 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
+var userName = $('.userName');
+var submit0 = $('.submit0');
+var welcomeCard = $('.welcomeCard');
 
-    <title></title>
-  </head>
-  <body>
-    <!-- where the header goes// on every single page -->
-        <div class="header-all">
-        <h1 class="title-all">HELLO</h1>
-        <div class="menu-all">
-          <a class="home" href="">Home</a>
-          <a class="about" href="">About</a>
-          <a class="tracker" href="">Personal Tracker</a>
-          <a class="resources" href="">Resources</a>
-        </div>
-        </div>
+var todayTrackerContainer = $('.todayTrackerContainer');
+var submit = $('.submitBtn');
 
+submit0.on("click", welcomeName);
+submit.on("click", createNewContainer);
+$('.todayTrackerContainer').on("click", ".delete-btn", deleteItem);
+$('.welcomeCard').on("click", ".resetName", deleteItem);
 
-    <div class = "name-input">
-      <input class = "userName" type = "text" placeholder = "name">
-      <button class = "submit0">Submit<button>
-    </div>
+// prints out welcome, (name) after user presses submit0 button
+function welcomeName() {
+  var userName = $('.userName').val();
 
+  appendWelcomeCard(userName);
+  storeName(userName);
+  clearInputs0();
+}
 
-    <!-- user inputs their volunteer info here -->
-    <div class = "trackerInput">
-      <h3> Log Your Volunteer Experience! </h3>
-      <h4>Organization</h4>
-      <input class = "organization" type = "text" placeholder = "Organization">
+function appendWelcomeCard(userName) {
+  welcomeCard.append(`
+                      <p class = "welcomeUser"> Welcome, ${userName}!
+                      <button class = "resetName">Reset</button>
+                      `);
 
-      <h4>Date</h4>
-      <input class = "date" type = "date">
+  console.log('user submitted name');
+}
 
-      <h4>Amount of Hours</h4>
-      <input class = "hours" type = "number">
+function storeName(userName) {
+  userName.preventDefault();
+  var name = $('.userName').val();
+  localStorage.clear();
+  localStorage.setItem('name', name);
+}
 
-      <h4>Point of Contact</h4>
-      <input class = "contactInfo" type = "text" placeholder= "Name">
-      <input class = "contactInfo2" type = "text" placeholder= "Email">
-      <input class = "contactInfo3" type = "text" placeholder= "Phone Number">
-
-      <h4>Reflection (Optional)</h4>
-      <input class = "reflection" type = "text" placeholder = "What was your experience like?">
-
-      <!-- ideally this question: <h4>How did you feel about your experience today? </h4>
-    would be answered by the user clicking an emoji button and it'll somehow export that to
-  the containers/past logs-->
-
-      <button class = "submitBtn"> Submit </button>
-    </div>
+function clearInputs0(event){
+  $('.userName').val("");
+}
 
 
-    <!-- user's today info appears below in another box -->
-    <div class = "pastLogs">
-      <div class = "todayTrackerContainer">
-        <h3> Past Logs </h3>
-        <button class="left-btn">&laquo; Previous</button>
-        <input class = "pastLogDate" type = "date">
-        <button class="right-btn">Next &raquo;</button>
-        <!-- empty until user inputs something -->
-      </div>
-    </div>
 
+// prints out the info put into the tracker on a log below
+function createNewContainer() {
+  var newOrg = $('.organization').val();
+  var newDate = $('.date').val();
+  var newHours = $('.hours').val();
+  var newContactInfo = $('.contactInfo').val();
+  var newContactInfo2 = $('.contactInfo2').val();
+  var newContactInfo3 = $('.contactInfo3').val();
+  var newReflection = $('.reflection').val();
 
-<!--
-    calendar import
-    <script src="js/daypilot/daypilot-all.min.js" type="text/javascript"></script>
+  var newLog = {newOrg: newOrg,
+                newDate: newDate,
+                newHours: newHours,
+                newContactInfo: newContactInfo,
+                newContactInfo2: newContactInfo2,
+                newContactInfo3: newContactInfo3,
+                newReflection: newReflection};
 
-    event calendar import
-    <script type="text/javascript">
-      var dp = new DayPilot.Calendar("dp");
-      dp.viewType = "Week";
-      dp.init();
-    </script>
+  appendNewLog(newLog);
+  clearInputs();
+}
 
-    event calendar placeholder
-    <div id="dp">
-      empty for now until js
-    </div> -->
+function appendNewLog(newLog){
+  todayTrackerContainer.append(`
+                                <div class = "new-log-card">
+                                  <h4 class = "orgName-card"> Organization: </h4> <p> ${newLog.newOrg} </p>
+                                  <h4 class = "date-card"> Date: </h4> <p> ${newLog.newDate} </p>
+                                  <h4 class = "hour-card"> Amount of Hours: </h4> <p> ${newLog.newHours} </p>
+                                  <h4 class = "contact-card"> Point of Contact: </h4>
+                                  <h4 class = "name-card"> Name: </h4> <p> ${newLog.newContactInfo} </p>
+                                  <h4 class = "email-card"> Email: </h4> <p> ${newLog.newContactInfo2} </p>
+                                  <h4 class = "number-card"> Phone Number: </h4> <p> ${newLog.newContactInfo3} </p>
+                                  <h4 class = "reflection-card"> Reflection: </h4> <p>${newLog.newReflection}</p>
+                                  <button class="delete-btn">Delete</button>
+                                </div>
+                              `);
+}
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src = 'tracker.js'></script>
-  </body>
-</html>
+function deleteItem(event){
+  event.target.parentNode.remove();
+}
+
+// clears the inputs on the tracker that the user input
+function clearInputs(){
+  $('.organization').val("");
+  $('.date').val("");
+  $('.contactInfo').val("");
+  $('.contactInfo2').val("");
+  $('.contactInfo3').val("");
+  $('.reflection').val("");
+}
